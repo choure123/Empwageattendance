84e1de75e8ca186827a3f905903a9f94a79237b7
sonu choure <131000134+choure123@users.noreply.github.com>
2023-05-15 23:41:57 +0530

    ADDED: Dailywagecalculator

---
+++ src/DailyCalculator.java
@@ -0,0 +1,10 @@
+public class DailyCalculator {
+    public static void main(String[] args) {
+        int A=20;
+        int b=8;
+        //a= wageper hour and b = fulldayhour
+        int dailywage= A * b;
+        System.out.println("Daily wage of the employee is " + dailywage);
+    }
+
+}
