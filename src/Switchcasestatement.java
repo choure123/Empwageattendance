9e9d0dae2f29ac62ae4dd001757d9fa4db02b77e
sonu choure <131000134+choure123@users.noreply.github.com>
2023-05-16 00:24:04 +0530

    ADDED DAYOFWEEK

---
+++ src/Switchcasestatement.java
@@ -0,0 +1,38 @@
+public class Switchcasestatement {
+    public static void main(String[] args) {
+        int dayofweek =2;
+        switch (dayofweek){
+            case 1:
+                System.out.println( "monday");
+                break;
+            case 2:
+                System.out.println( "tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+
+            case 6:
+                System.out.println("satarday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println( "uhh wrong day of week");
+                break;
+
+
+
+
+
+        }
+    }
+}
